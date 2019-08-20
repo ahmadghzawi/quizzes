c70433742a5c47b7e7a4af82c05bca8480ad55c7
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-08-20 09:33:20 +0300

    add files after reorganizing folders

---
+++ July/quiz/all-tide-up.js
@@ -0,0 +1,26 @@
+/*
+ * Programming Quiz: All Tied Up 
+ * Build a single string that resembles the following joke.
+ *
+ * Why couldn't the shoes go out and play?
+ * They were all "tied" up!
+ * Your joke should take the format of a question and answer. 
+ * The first line should be a question and the second line should be an answer.
+ * Hint: You will need to use special characters to produce the following output.*/
+
+ var jokeQuestion;
+ var jokeAnswer;
+
+ jokeQuestion = "Question: " + "Why couldn't the shoes go out and play? \n";
+ jokeAnswer = "Answer: " + "They were all \"tied\" up!";
+
+ console.log(jokeQuestion);
+ console.log(jokeAnswer);
+
+ //OR
+
+var joke;
+
+joke = "Question: " + "Why couldn't the shoes go out and play? \nAnswer: " + "They were all \"tied\" up!";
+
+console.log(joke);
---
+++ July/quiz/expression.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: First Expression 
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+
+var num = ((90 - 9 + 3)/4) * 2;
+
+console.log(num);
